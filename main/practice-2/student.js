2a92f9121a5cfd76a3f5b86ca36f24fe3db9e279
winar-jin <winar1994@outlook.com>
2017-08-19 08:25:05 +0800

    update the code

--- main/practice-2/student.js
+++ main/practice-2/student.js
@@ -11,7 +11,6 @@
 const Person = require('./person');
 
 class Student extends Person {
-    //这里 name,age,clazz,id 的顺序不能错，因为在 main-spec.js 中调用 Student 时传入参数是有一定顺序的
     constructor(name, age, clazz, id) {
         super(name, age, id);
         this.clazz = clazz;
